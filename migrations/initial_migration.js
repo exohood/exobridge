962f78bf5f84e661662ef178326e6ac628f87aae
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-09-11 12:55:58 +0200

    Create initial_migration.js

---
+++ migrations/initial_migration.js
@@ -0,0 +1,41 @@
+var Migrations = artifacts.require("./Migrations.sol");
+var Bridge = artifacts.require('./Bridge.sol');
+var ExoLib = artifacts.require('./MultichainProof.sol');
+var RLP = artifacts.require('./RLP.sol');
+var EIP20 = artifacts.require('EIP20.sol');
+
+const argv = require('yargs')
+  .usage('Usage: <cmd> [options]')
+  .command('--live', 'Deploy a specific bridge with a stake token')
+  .command('--token', 'Staking token to deploy on the bridge. If none is provided, deploy a new token.')
+  .command('--MuPoS', 'Multichain Proof of Stake consensus mechanism.')
+  .argv;
+
+// seems redundant, argv.live unused (e.g. does NOT deploy a specic bridge as mentioned above)
+if (argv.live) {
+  module.exports = function(deployer) {
+    deployer.deploy(Migrations);
+    deployer.deploy(MerkleLib);
+    deployer.link(MerkleLib, Bridge);
+    deployer.deploy(RLP);
+    deployer.link(RLP, MerkleLib);
+    if (argv.token && argv.token != '') {
+      deployer.deploy(Bridge, argv.token);
+    } else {
+      deployer.deploy(EIP20, 1000000000000, 'Staking', 0, 'EXO')
+      .then(() => { return deployer.deploy(Bridge, EIP20.address); })
+    }
+  }
+} else {
+  // TODO: code is neraly redundant
+  module.exports = function(deployer) {
+    deployer.deploy(Migrations);
+    deployer.deploy(MerkleLib);
+    deployer.link(MerkleLib, Bridge);
+    deployer.deploy(RLP);
+    deployer.link(RLP, MerkleLib);
+    // do not call without constructor param, since truffle 4.0.6 this throws an error
+    deployer.deploy(EIP20, 1000000000000, 'Staking', 0, 'EXO')
+    .then(() => { return deployer.deploy(Bridge, EIP20.address); })
+  };
+};
